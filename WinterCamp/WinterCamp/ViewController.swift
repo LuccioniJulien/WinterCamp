c850b0ea373fd16dbc75e4b164b6c12b02357a61
Unknown <julien.luccioni@protonmail.com>
2018-02-15 10:28:09 +0100

    First commit
    initial commit

---
+++ WinterCamp/WinterCamp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WinterCamp
+//
+//  Created by Julien Luccioni on 15/02/2018.
+//  Copyright © 2018 Julien Luccioni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
